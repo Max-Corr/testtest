78ea58c9ac5438507d788b590c58ba0a2f587b67
Max-Corr <83875068+Max-Corr@users.noreply.github.com>
2024-02-02 21:03:50 +0300

    Add files via upload

---
+++ ryjgrf (2).py
@@ -0,0 +1,17 @@
+from tkinter import *
+import webbrowser as pprrrrr91
+
+root = Tk()
+
+root.geometry('230x479')
+
+root.geometry('230x479')
+
+def perexod():
+
+    pprrrrr91.open('https://thewikihow.com/video_gw5SxNnoOPg')
+
+kl = Button(text="бесплатные монеты от Михаила Литвина-11-11", command=perexod)
+
+kl.pack()
+root.mainloop()
